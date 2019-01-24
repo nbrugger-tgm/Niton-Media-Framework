e186ee6e4e81dba7955f4c3c3f50e7e4f93acb29
Nils Brugger <34156963+nbrugger-tgm@users.noreply.github.com>
2019-01-24 09:56:09 +0100

    Exception forwarding for AES encryption

--- src/com/niton/media/crypt/SimpleAES.java
+++ src/com/niton/media/crypt/SimpleAES.java
@@ -27,7 +27,7 @@ public class SimpleAES {
 	private static final int pad = 16;
 	public static SecretKey lastKey;
 	
-	public static byte[] encrypt(SecretKey key, byte[] dataToEncrypt) {
+	public static byte[] encrypt(SecretKey key, byte[] dataToEncrypt) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
 		int padding = pad-(dataToEncrypt.length%pad);
 		byte[] padded = new byte[dataToEncrypt.length + padding];
 		Random r = new Random();
@@ -38,8 +38,13 @@ public class SimpleAES {
 		return doCrypto(Cipher.ENCRYPT_MODE, key, padded);
 	}
 
-	public static byte[] decrypt(SecretKey key, byte[] inputFile) {
-		byte[] dec = doCrypto(Cipher.DECRYPT_MODE, key, inputFile);
+	public static byte[] decrypt(SecretKey key, byte[] encryptedData) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException {
+		byte[] dec;
+		try {
+			dec = doCrypto(Cipher.DECRYPT_MODE, key, encryptedData);
+		} catch (NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException e) {
+			return null;
+		}
 		byte[] ret = new byte[dec.length - (dec[dec.length - 1])];
 		for (int i = 0; i < ret.length; i++) {
 			ret[i] = dec[i];
@@ -47,17 +52,12 @@ public class SimpleAES {
 		return ret;
 	}
 
-	private static byte[] doCrypto(int cipherMode, SecretKey key, byte[] data) {
-		try {
-			Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-			IvParameterSpec ivspec = new IvParameterSpec(IV);
-			cipher.init(cipherMode, key, ivspec);
-			byte[] outputBytes = cipher.doFinal(data);
-			return outputBytes;
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			return new byte[0];
-		}
+	private static byte[] doCrypto(int cipherMode, SecretKey key, byte[] data) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+		Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+		IvParameterSpec ivspec = new IvParameterSpec(IV);
+		cipher.init(cipherMode, key, ivspec);
+		byte[] outputBytes = cipher.doFinal(data);
+		return outputBytes;
 	}
 
 	public static SecretKey generateKey(int size) {
